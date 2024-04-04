5fedc76cb4bb26cd572f8d78b2431e1172edca05
Austen Goodson <austen@goodsons.net>
2024-04-04 13:03:23 -0700

    feat: make sure load method only runs on the client

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,9 @@
 import type { TimerType } from "$lib/types";
 import { browser } from '$app/environment';
 
+// Ensures that the load function only loads on the client side
+export const ssr = false;
+
 function generateUniqueId() {
     return Math.random().toString(36).slice(2, 9);
 }
@@ -10,25 +13,27 @@ export async function load() {
 
     // Make sure we're in the browser
     if (browser) {
+        console.log('Loading timers from local storage')
         // If we are, load the timers from local storage - if they exist.
         const storedTimers = localStorage.getItem('timers');
         timers = storedTimers ? JSON.parse(storedTimers) : [];
+        if (timers.length === 0) {
+            // Initialize the timers with the default timer
+            timers = [
+                {
+                    timerLengthInSeconds: 63,
+                    timerName: 'Default',
+                    timerId: generateUniqueId(),
+                    linked: true
+                }
+            ]
+        }
+        return {
+            timers: timers
+        };
     }
-    if (timers.length === 0) {
-        // Initialize the timers with the default timer
-        timers = [
-            {
-                timerLengthInSeconds: 60,
-                timerName: 'Default',
-                timerId: generateUniqueId(),
-                linked: true
-            }
-        ]
-    }
-
 
     return {
-        timers: timers || []
-
-    };
+        timers: []
+    }
 }
