e94e7316e0e711b78536f7c0356ba8802bb932c7
Austen Goodson <austen@goodsons.net>
2024-04-04 10:48:10 -0700

    feat: add sound and reset timer when ends

--- src/lib/components/Timer.svelte
+++ src/lib/components/Timer.svelte
@@ -1,12 +1,20 @@
 <svelte:options accessors={true} />
 
 <script lang="ts">
+	import { Sound } from 'svelte-sound';
+	import tonalBeep from '$lib/sounds/tonal_beep.wav';
+
 	import Plus from '$lib/icons/Plus.svelte';
 	import Minus from '$lib/icons/Minus.svelte';
 	import Play from '$lib/icons/Play.svelte';
 	import Pause from '$lib/icons/Pause.svelte';
 	import ArrowPath from '$lib/icons/ArrowPath.svelte';
 	import Trash from '$lib/icons/Trash.svelte';
+
+	const beepSound = new Sound(tonalBeep);
+
+	// The time in milliseconds to play an alert sounds or flashings
+	const alertTime = 3000;
 
 	export let intervalTime = 60; // 1 minute - the time to add or subtract from the timer
 	export let timerId: string;
@@ -38,9 +46,33 @@
 	$: timerHasEnded = timeLeft === 0 && timerHasStarted;
 	let intervalId: ReturnType<typeof setTimeout>;
 	$: {
+		// Logic to handle the timer ending
 		if (timerHasEnded) {
+			// Play the alerts
+			playAlert();
+			// Let the parent component konw that the timer has ended
 			timerEnded(timerId);
+			// Reset the timer
+			resetCountdown();
 		}
+	}
+
+	// Indicates whether the alert text should be showing or not
+	let activeAlertText: boolean = false;
+	function playAlert() {
+		// Play a sound when the timer ends
+		beepSound.play();
+		// Set an interval to turn off the sound
+		setTimeout(() => {
+			beepSound.stop();
+		}, alertTime);
+		let intervalId = setInterval(() => {
+			activeAlertText = !activeAlertText;
+		}, 200);
+		setTimeout(() => {
+			clearInterval(intervalId);
+			activeAlertText = false;
+		}, alertTime);
 	}
 
 	export let startCountdown = () => {
@@ -79,7 +111,9 @@
 		<button on:click={() => deleteTimer(timerId)}><Trash /></button>
 	{/if}
 	<div class="flex flex-col justify-center items-center grow">
-		<div class="text-7xl">{convertTimeToString(timeLeft)}</div>
+		<div class={`text-7xl ${activeAlertText ? 'text-accent' : ''}`}>
+			{convertTimeToString(timeLeft)}
+		</div>
 		<div class="flex flex-row items-center">
 			<div class="badge badge-outline mr-1">{convertTimeToString(timeInSeconds)}</div>
 			<button on:click={resetCountdown}><ArrowPath /></button>
