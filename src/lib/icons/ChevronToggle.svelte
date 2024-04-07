7574636600e7a5383b21bbe606bcc84ca039241d
Austen Goodson <austen@goodsons.net>
2024-04-07 12:29:14 -0700

    feat: change the icon for indicating timers are chained together

---
+++ src/lib/icons/ChevronToggle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ChevronDoubleDown from './ChevronDoubleDown.svelte';
+	import DoubleBars from './DoubleBars.svelte';
+
+	export let active = true;
+</script>
+
+{#if active}
+	<ChevronDoubleDown {active} />
+{:else}
+	<DoubleBars />
+{/if}
